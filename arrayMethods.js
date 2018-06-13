2e783c974f5cfbf6a9677c39e25fa285f95363c0
Sarah <yamashige.sarah@gmail.com>
2018-06-12 20:22:22 -1000

    working on replacing with splice

--- arrayMethods.js
+++ arrayMethods.js
@@ -39,5 +39,10 @@ mixedWords.sort();
 
 //splice() 
 var notFruit = fruitCollection.splice(1,3);
+gemBox.splice(5,7, "Sapphire");
+
+//splice() - inserting elements
+upToTen.splice(2,0, 3,4,5,6,7,8);
+brownOnly.splice()
 
 
